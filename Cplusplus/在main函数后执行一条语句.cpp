305be770ddf7cc9b071cc1d724be2e5a6d22cb9c
LzyRapx <LzyRapx@gmail.com>
2018-08-05 23:05:38 +0800

    在main函数后执行一条语句

---
+++ Cplusplus/在main函数后执行一条语句.cpp
@@ -0,0 +1,24 @@
+```
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+/*
+atexit()函数是在函数正常终止之前被调用执行的函数,
+而且执行顺序和调用顺序正好相反。
+原型： int atexit( void (*)(void) );
+在一个函数中最多可以用 atexit注册32个函数。
+*/
+int atexit(void (*function)(void));
+
+void gao(){
+  printf( "This is executed last.\n" );
+}
+
+int main(){
+    atexit( gao );
+    printf( "This is executed first.\n" );
+    return 0;
+}
+
+```
